a9773b4b2cc8fbdddcf782229956121d25feec69
= <=>
2022-09-09 22:12:00 -0500

    boostsrap nav bars

---
+++ booksnewlife/booksnewlife/urls.py
@@ -0,0 +1,9 @@
+from . import view
+
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("",view.index,name='index')
+]
