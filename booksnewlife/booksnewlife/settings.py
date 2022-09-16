001f33d90ab7837bc7e221e74fce787e276c23cc
= <=>
2022-09-15 22:18:54 -0500

    mysql

--- booksnewlife/booksnewlife/settings.py
+++ booksnewlife/booksnewlife/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = "booksnewlife.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME":"books",
+        "USER":'root',
+        'PASSWORD':'123456789',
+        'HOST':'localhost',
+        'PORT':''
+        
     }
 }
 
--- booksnewlife/login/models.py
+++ booksnewlife/login/models.py
@@ -1,3 +1,13 @@
+from xml.parsers.expat import model
 from django.db import models
 
-# Create your models here.
+class DatosUsuarios(models.Model):
+    
+    nombre=models.CharField(max_length=30,null=False)
+    apellido=models.CharField(max_length=30,null=False)
+    correo=models.CharField(max_length=50,null=False)
+    contraseña=models.CharField(max_length=50,null=False)
+    def __str__(self):
+        return self.name
+    
+
