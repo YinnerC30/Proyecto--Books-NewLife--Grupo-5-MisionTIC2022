bc55653b4ccb320055e5440bfc95b27d4a410753
Yinner Chilito <yinnermedina30@gmail.com>
2022-09-29 21:51:45 -0500

    interfaces-segundo-ajuste

--- books_new_life/feedback/models.py
+++ books_new_life/feedback/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # Create your models here.
 class Comentario(models.Model):
     nombre = models.CharField(max_length=100)
-    correo = models.EmailField()
+    correo = models.EmailField(max_length=50)
     departamento = models.CharField(max_length=100)
-    mensaje = models.TextField()
-    fecha = models.DateTimeField(auto_now_add=True)
+    mensaje = models.CharField(max_length=300)
 
     def __str__(self):
         return f"Nombre: {self.nombre} | Departamento: {self.departamento}"
--- books_new_life/miembros/forms.py
+++ books_new_life/miembros/forms.py
@@ -1,45 +1,54 @@
 
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
 
 
 class SignUpForm(UserCreationForm):
 
-    username = forms.CharField(max_length=100, label='', widget=forms.TextInput(
-        attrs={'placeholder': 'Usuario', 'class': 'signup-input'}), )
+    username = forms.CharField(max_length=100, label='Nombre de usuario:', widget=forms.TextInput(
+        attrs={'placeholder': 'Nombre de usuario', 'class': 'input-control'}), )
 
-    email = forms.EmailField(label='', widget=forms.EmailInput(
-        attrs={'placeholder': 'Correo', 'class': 'signup-input'}))
+    email = forms.EmailField(label='Correo electronico:', widget=forms.EmailInput(
+        attrs={'placeholder': 'Correo electronico', 'class': 'input-control'}))
 
-    first_name = forms.CharField(max_length=100, label='', widget=forms.TextInput(
-        attrs={'placeholder': 'Nombre', 'class': 'signup-input'}))
+    first_name = forms.CharField(max_length=100, label='Nombre:', widget=forms.TextInput(
+        attrs={'placeholder': 'Nombre', 'class': 'input-control'}))
 
-    last_name = forms.CharField(max_length=100, label='', widget=forms.TextInput(
-        attrs={'placeholder': 'Apellido', 'class': 'signup-input'}))
+    last_name = forms.CharField(max_length=100, label='Apellido:', widget=forms.TextInput(
+        attrs={'placeholder': 'Apellido', 'class': 'input-control'}))
 
-    password1 = forms.CharField(max_length=100, label='', widget=forms.PasswordInput(
-        attrs={'placeholder': 'Contraseña', 'class': 'signup-input'}))
+    password1 = forms.CharField(max_length=100, label='Contraseña:', widget=forms.PasswordInput(
+        attrs={'placeholder': 'Contraseña', 'class': 'input-control'}))
 
-    password2 = forms.CharField(max_length=100, label='', widget=forms.PasswordInput(
-        attrs={'placeholder': 'Repetir contraseña', 'class': 'signup-input'}))
+    password2 = forms.CharField(max_length=100, label='Repetir contraseña:', widget=forms.PasswordInput(
+        attrs={'placeholder': 'Repetir contraseña', 'class': 'input-control'}))
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name',
                   'email', 'password1', 'password2')
+            
 
 
 class UserEditForm(UserChangeForm):
-    username = forms.CharField(max_length=100, label='Usuario', widget=forms.TextInput(
-        attrs={'placeholder': 'Usuario', 'class': 'signup-input'}), )
+    username = forms.CharField(max_length=100, label='Usuario', widget=forms.TextInput(attrs={'placeholder': 'Usuario', 'class': 'box'}), )
     first_name = forms.CharField(max_length=100, label='Nombre', widget=forms.TextInput(
-        attrs={'placeholder': 'Nombre', 'class': 'signup-input'}), )
+        attrs={'placeholder': 'Nombre', 'class': 'box'}), )
     last_name = forms.CharField(max_length=100, label='Apellidos', widget=forms.TextInput(
-        attrs={'placeholder': 'Apellidos', 'class': 'signup-input'}), )
+        attrs={'placeholder': 'Apellidos', 'class': 'box'}), )
     email = forms.EmailField(label='Email', widget=forms.EmailInput(
-        attrs={'placeholder': 'Correo', 'class': 'signup-input'}))
+        attrs={'placeholder': 'Correo', 'class': 'box'}))
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password')
+        fields = ('username', 'first_name', 'last_name', 'email','password')
+
+# class LoginForm(AuthenticationForm):
+#     username = forms.CharField(max_length=100, label='Usuario', widget=forms.TextInput(attrs={'placeholder': 'Usuario', 'class': 'box'}), )
+#     password = forms.CharField(max_length=100, label='Contraseña', widget=forms.PasswordInput(
+#         attrs={'placeholder': 'Contraseña', 'class': 'box'}), )
+
+#     class Meta:
+#         model = User
+#         fields = ('username', 'password')
